8f8bae45ebb392fc395751aeb30b1a73a5235687
InnarOol <innar74@gmail.com>
2023-12-15 18:46:45 +0200

    delete_pom

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -1,25 +1,59 @@
 /**
  * This is an example file and approach for POM in Cypress
  */
-import IssueModal from "../../pages/IssueModal";
+import IssueModal from "../../pages/IssueModal.js";
 
-describe('Issue delete', () => {
+// Describe the test suite for issue deletion
+describe('Issue Deletion Test Suite', () => {
+  
+  // Define the issue title we are testing with
+  const issueTitle = 'This is an issue of type: Task.';
+
+  // Define the setup steps to run before each test
   beforeEach(() => {
+    
+    // Visit the Jira board page
     cy.visit('/');
+    
+    // Verify the URL to ensure we are on the correct page
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
-    //open issue detail modal with title from line 16  
-    cy.contains(issueTitle).click();
+      // Open the issue detail modal by clicking on the issue title
+      cy.contains(issueTitle).click();
     });
   });
 
-  //issue title, that we are testing with, saved into variable
-  const issueTitle = 'This is an issue of type: Task.';
-
-  it('Should delete issue successfully', () => {
-    //add steps to delete issue
+  // Test case: Should delete issue successfully
+  it('Test Case 1: Delete Issue', () => {
+    
+    // Get the issue detail modal
+    IssueModal.getIssueDetailModal() 
+    
+    // Click the delete button
+    IssueModal.clickDeleteButton()
+    
+    // Confirm the deletion
+    IssueModal.confirmDeletion()
+    
+    // Ensure the issue is not visible on the board
+    IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle)
   });
 
-  it('Should cancel deletion process successfully', () => {
-    //add steps to start deletion proces but cancel it
+  // Test case: Should cancel deletion process successfully
+  it('Test Case 2: Cancel Deletion', () => {
+    
+    // Get the issue detail modal
+    IssueModal.getIssueDetailModal() 
+    
+    // Click the delete button
+    IssueModal.clickDeleteButton()
+    
+    // Cancel the deletion
+    IssueModal.cancelDeletion()
+    
+    // Close the issue detail modal
+    IssueModal.closeDetailModal()
+    
+    // Ensure the issue is still visible on the board
+    IssueModal.ensureIssueIsVisibleOnBoard(issueTitle)
   });
 });
