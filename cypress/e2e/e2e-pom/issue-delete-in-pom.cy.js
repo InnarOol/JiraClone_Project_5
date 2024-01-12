c1284b1b3df08083a764e634f2aef79c11fe0f6f
InnarOol <innar74@gmail.com>
2024-01-12 20:59:38 +0200

    ok

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -5,16 +5,12 @@ import IssueModal from "../../pages/IssueModal.js";
 
 // Describe the test suite for issue deletion
 describe('Issue Deletion Test Suite', () => {
-  
   // Define the issue title we are testing with
   const issueTitle = 'This is an issue of type: Task.';
-
   // Define the setup steps to run before each test
   beforeEach(() => {
-    
     // Visit the Jira board page
     cy.visit('/');
-    
     // Verify the URL to ensure we are on the correct page
     cy.url().should('eq', `${Cypress.env('baseUrl')}project/board`).then((url) => {
       // Open the issue detail modal by clicking on the issue title
@@ -24,16 +20,12 @@ describe('Issue Deletion Test Suite', () => {
 
   // Test case: Should delete issue successfully
   it('Test Case 1: Delete Issue', () => {
-    
     // Get the issue detail modal
     IssueModal.getIssueDetailModal() 
-    
     // Click the delete button
     IssueModal.clickDeleteButton()
-    
     // Confirm the deletion
     IssueModal.confirmDeletion()
-    
     // Ensure the issue is not visible on the board
     IssueModal.ensureIssueIsNotVisibleOnBoard(issueTitle)
   });
@@ -43,16 +35,12 @@ describe('Issue Deletion Test Suite', () => {
     
     // Get the issue detail modal
     IssueModal.getIssueDetailModal() 
-    
     // Click the delete button
     IssueModal.clickDeleteButton()
-    
     // Cancel the deletion
     IssueModal.cancelDeletion()
-    
     // Close the issue detail modal
     IssueModal.closeDetailModal()
-    
     // Ensure the issue is still visible on the board
     IssueModal.ensureIssueIsVisibleOnBoard(issueTitle)
   });
